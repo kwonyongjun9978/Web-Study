43f40e4b194642adae07a661679112e26a7f4629
Jun <rnsuz74@naver.com>
2023-03-21 21:10:54 +0900

    mvc

--- mvcMember/src/main/java/com/control/CommandProcess.java
+++ mvcMember/src/main/java/com/control/CommandProcess.java
@@ -4,5 +4,5 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface CommandProcess {
-	public String requestPro(HttpServletRequest request, HttpServletResponse response);
+	public String requestPro(HttpServletRequest request, HttpServletResponse response); //추상메소드
 }
